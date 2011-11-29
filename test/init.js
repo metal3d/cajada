920bac22b75d804d2602a19c6c5ac8c42af918bf
Patrice FERLET <metal3d@gmail.com>
2011-11-29 01:30:34 +0100

    Better documentation on test

--- test/init.js
+++ test/init.js
@@ -1,43 +1,30 @@
-//Custom shape, extends a shape
-/*
-(function (){
-    myShape.prototype = new cajada.Shapes.Base();
-    myShape.prototype.constructor = function (c, options){
-        cajada.Shapes.Rect(c, options);
-    };
 
-    myShape.prototype.draw = function () {
-        cajada.Shapes.RoundRect.draw();
-        this.begin();
-        this.scene.ctx.fillStyle = "bkack";
-        this.scene.ctx.filleText(this.x, this.y);
-        this.end();
-        return this;
-    };
 
-    var m = new myShape(c, {
-        at: [150,200]
-    });
-    console.log(myShape);
-})();
-*/
-  
+//simple getElement
 function $(element) {
     return document.getElementById(element);
 }
 
+
+//called on load, see last line
+
 function letsGo() {
+    
+    //create a scene from canvas with 'board' id
     var c = new cajada.Scene({
         canvas: $('board')
     });
 
 
 
+    //A simple rectangle
     var r1 = new cajada.Shapes.Rect(c, {
         at : [90,30],
         size: [150,80],
         strokeStyle:"blue"
     });
+    
+    //a rounded rectangle
     var r2 = new cajada.Shapes.RoundedRect(c, {
         at: [120,90],
         size: [70,100],
@@ -46,14 +33,16 @@ function letsGo() {
         rotation: 90
     });
 
+    //on rounder rectangle, add text by this custrom method
     r2.addCustomDraw(function (){
         var ctx = r2.scene.ctx;
         ctx.font="10pt Sans";
         ctx.textAlign="center";
         ctx.fillStyle="black";
-        ctx.fillText('Youpi', 0, 0);
+        ctx.fillText('Youpi', 0, 0); // 0,0 coord because context is translate to the center of shape
     });
 
+    //when canvas is refreshed, draw a line beetween r1 and r2
     c.addEventListener('refresh', function (){
         var ctx = c.ctx;
         ctx.save();
@@ -66,11 +55,8 @@ function letsGo() {
         ctx.restore();
     });
 
-    /*
-    r2.addEventListener('click', function (){
-        console.log('event click captured');    
-    });
-    */
+    //A nice view, if mouse is over r2, fill it with a
+    //semi transparent green color
     r2.addEventListener('mouseover', function (){
         r2.lastFill = r2.options.fillStyle;
         r2.options.fillStyle = "rgba(0,255,0,0.9)";
@@ -82,6 +68,7 @@ function letsGo() {
     });
 
 
+    //change index on mouse down
     r1.addEventListener('mousedown', function (){
         r1.zindex('up');
     });
@@ -90,29 +77,13 @@ function letsGo() {
         r2.zindex('up');
     });
 
+    //set rectangles draggables
     r1.setDraggable(true);
     r2.setDraggable();
 
+    //maybe nested
     c.refresh();
-    console.log(r2);
-    //d(c,r1,0,0,0,r2);
 
-}
-   
-function d (c,r1,t,X,Y,r2) {
-    if (t === 0 ) {
-        X = r1.x;
-        Y = r1.y;
-    }
-    c.refresh();
-    x = 80*Math.sin(t*Math.PI/2);
-    y = 4*Math.sin(t*Math.PI/2);
-    t += (t==1) ? -t : 0.01;
-    r1.x = X+x;
-    r1.y = Y+y;
-    r1.rotation+=0.1;
-    r2.rotation-=0.1; 
-    setTimeout( function () { d(c,r1,t,X,Y,r2); }, 1000/25);
 }
 
 window.onload = letsGo;
