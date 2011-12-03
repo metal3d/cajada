ef83a4c7e4192d63c4ee199949382aeb68a97ce6
Patrice FERLET <metal3d@gmail.com>
2011-12-03 18:40:40 +0100

    Same as last commit, no need this test

--- src/js/shapes.js
+++ src/js/shapes.js
@@ -19,8 +19,6 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-var cajada = cajada || {};
-
 cajada.Shapes =  (function (){
     //Shape interface that allows:
     // move, fill, stroke, styles...
